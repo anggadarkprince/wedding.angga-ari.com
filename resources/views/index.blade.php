746ef01aa1eb08960315a0c3d6d02ec79b426365
Angga Ari Wijaya <anggadarkprince@gmail.com>
2019-12-22 20:40:15 +0700

    fix url asset

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,19 +11,19 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Great+Vibes&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Libre+Caslon+Text:400,400i,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="{{ url('css/open-iconic-bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/animate.css') }}">
 
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="{{ url('css/owl.carousel.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/owl.theme.default.min.css') }}">
+    <link rel="stylesheet" href="{{ url('css/magnific-popup.css') }}">
 
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ url('css/aos.css') }}">
+    <link rel="stylesheet" href="{{ url('css/ionicons.min.css') }}">
 
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ url('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ url('css/icomoon.css') }}">
+    <link rel="stylesheet" href="{{ url('css/style.css') }}">
 </head>
 
 <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
@@ -716,21 +716,21 @@
     </div>
 
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/aos.js"></script>
-    <script src="js/jquery.animateNumber.min.js"></script>
-    <script src="js/jquery.mb.YTPlayer.min.js"></script>
-    <script src="js/scrollax.min.js"></script>
+    <script src="{{ url('js/jquery.min.js') }}"></script>
+    <script src="{{ url('js/jquery-migrate-3.0.1.min.js') }}"></script>
+    <script src="{{ url('js/popper.min.js') }}"></script>
+    <script src="{{ url('js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('js/jquery.easing.1.3.js') }}"></script>
+    <script src="{{ url('js/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ url('js/jquery.stellar.min.js') }}"></script>
+    <script src="{{ url('js/owl.carousel.min.js') }}"></script>
+    <script src="{{ url('js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ url('js/aos.js') }}"></script>
+    <script src="{{ url('js/jquery.animateNumber.min.js') }}"></script>
+    <script src="{{ url('js/jquery.mb.YTPlayer.min.js') }}"></script>
+    <script src="{{ url('js/scrollax.min.js') }}"></script>
 
-    <script src="js/main.js"></script>
+    <script src="{{ url('js/app.js') }}"></script>
 
 </body>
 
